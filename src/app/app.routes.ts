0280d09b47ae693c0e3508f50134420f89f6d434
deltron9 <cristian_collante@hotmail.com>
2026-04-14 04:35:32 -0300

    add: ruteo y diseño de diferentes páginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { Bienvenida } from './pages/bienvenida/bienvenida'; 
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
+import { ErrorPage } from './pages/error-page/error-page';
+import { Perfil } from './pages/perfil/perfil';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'bienvenida', 
+    component: Bienvenida 
+  },
+  {
+    path: '',
+    redirectTo: 'bienvenida',
+    pathMatch: 'full'
+  },
+  { path: 'login', 
+    component: Login 
+  },
+  { path: 'register', 
+    component: Register 
+  },
+  {  path: 'perfil',
+    component: Perfil
+  },
+  { path: '**', 
+    component: ErrorPage 
+  }
+];
---
+++ src/app/pages/bienvenida/bienvenida.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bienvenida',
+  standalone: true,
+  imports: [],
+  templateUrl: './bienvenida.html',
+  styleUrl: './bienvenida.css',
+})
+export class Bienvenida {}
